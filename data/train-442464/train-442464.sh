1badc70ac9aca95d198fd54357d4da080911cf0e
ariffero <andrea.giovanni.riffero@cern.ch>
2025-07-09 01:38:51 +0200

    Add script to download train n. 442464

---
+++ data/train-442464/train-442464.sh
@@ -0,0 +1,7 @@
+# script to download the data from the output of the HY train n. 442464
+# the train ran UPCCandidateProducerOO on the dataset LHC25ae_cpass0_QC1
+alien_cp alien:///alice/cern.ch/user/a/alihyperloop/jobs/0234/hy_2346065/AOD/*/AO2D.root file://hy_2346065/
+alien_cp alien:///alice/cern.ch/user/a/alihyperloop/jobs/0234/hy_2346064/AOD/*/AO2D.root file://hy_2346064/
+alien_cp alien:///alice/cern.ch/user/a/alihyperloop/jobs/0234/hy_2346063/AOD/*/AO2D.root file://hy_2346063/
+alien_cp alien:///alice/cern.ch/user/a/alihyperloop/jobs/0234/hy_2346062/AOD/*/AO2D.root file://hy_2346062/
+alien_cp alien:///alice/cern.ch/user/a/alihyperloop/jobs/0234/hy_2346061/AOD/*/AO2D.root file://hy_2346061/
