375a4fc04cd48ccaf215724f2731c7f11cc39181
ariffero <andrea.giovanni.riffero@cern.ch>
2025-07-11 15:27:30 +0200

    new version of getLumi.cpp from Roman

--- lumi-hy/getLumi.cpp
+++ lumi-hy/getLumi.cpp
@@ -10,15 +10,23 @@ void getLumi(string filename = "AnalysedLumi.root"){
 	Double_t systunc = 0.039; // https://alice-notes.web.cern.ch/node/1515
 
 	if (f){
-		TH1F *hLumiTVX  = (TH1F*) f->Get("bc-selection-task/hLumiTVX");
-		TH1F *hLumiTCE  = (TH1F*) f->Get("bc-selection-task/hLumiTCE");
-		TH1F *hLumiZEM  = (TH1F*) f->Get("bc-selection-task/hLumiZEM");
-		TH1F *hLumiZNC  = (TH1F*) f->Get("bc-selection-task/hLumiZNC");
-		TH1F *hLumiTVXafterBCcuts  = (TH1F*) f->Get("bc-selection-task/hLumiTVXafterBCcuts");
-		TH1F *hLumiTCEafterBCcuts  = (TH1F*) f->Get("bc-selection-task/hLumiTCEafterBCcuts");
-		TH1F *hLumiZEMafterBCcuts  = (TH1F*) f->Get("bc-selection-task/hLumiZEMafterBCcuts");
-		TH1F *hLumiZNCafterBCcuts  = (TH1F*) f->Get("bc-selection-task/hLumiZNCafterBCcuts");
+		TH1F *hLumiTVX  = (TH1F*) f->Get("lumi-task/hLumiTVX");
+		TH1F *hLumiTCE  = (TH1F*) f->Get("lumi-task/hLumiTCE");
+		TH1F *hLumiZEM  = (TH1F*) f->Get("lumi-task/hLumiZEM");
+		TH1F *hLumiZNC  = (TH1F*) f->Get("lumi-task/hLumiZNC");
+		TH1F *hLumiTVXafterBCcuts  = (TH1F*) f->Get("lumi-task/hLumiTVXafterBCcuts");
+		TH1F *hLumiTCEafterBCcuts  = (TH1F*) f->Get("lumi-task/hLumiTCEafterBCcuts");
+		TH1F *hLumiZEMafterBCcuts  = (TH1F*) f->Get("lumi-task/hLumiZEMafterBCcuts");
+		TH1F *hLumiZNCafterBCcuts  = (TH1F*) f->Get("lumi-task/hLumiZNCafterBCcuts");
 		TH1F *hTriggerTCEupcCand  = (TH1F*) f->Get("upc-cand-producer/hCountersTrg");
+		TH1F *hCounterTVX  = (TH1F*) f->Get("lumi-task/hCounterTVX");
+		TH1F *hCounterTCE  = (TH1F*) f->Get("lumi-task/hCounterTCE");
+		TH1F *hCounterZEM  = (TH1F*) f->Get("lumi-task/hCounterZEM");
+		TH1F *hCounterZNC  = (TH1F*) f->Get("lumi-task/hCounterZNC");
+		TH1F *hCounterTVXafterBCcuts  = (TH1F*) f->Get("lumi-task/hCounterTVXafterBCcuts");
+		TH1F *hCounterTCEafterBCcuts  = (TH1F*) f->Get("lumi-task/hCounterTCEafterBCcuts");
+		TH1F *hCounterZEMafterBCcuts  = (TH1F*) f->Get("lumi-task/hCounterZEMafterBCcuts");
+		TH1F *hCounterZNCafterBCcuts  = (TH1F*) f->Get("lumi-task/hCounterZNCafterBCcuts");
 		if (hLumiTVX){
 			Double_t lumi = 0.0;
 			for (int ibin(1);ibin <= hLumiTVX->GetNbinsX();ibin++){
@@ -89,6 +97,70 @@ void getLumi(string filename = "AnalysedLumi.root"){
 			Printf("Total TCE triggers from UPCCandProducer: %.0f; Luminosity is: %.2f±%.2f /mub", triggers, lumi, systunc*lumi);
 		}
 		else Printf("no UPCCandProducer output");
+		if (hCounterTVX){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterTVX->GetNbinsX();ibin++){
+				lumi += hCounterTVX->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by TVX is: %.2f", lumi);
+		}
+		else Printf("no TVX triggers info");
+		if (hCounterTCE){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterTCE->GetNbinsX();ibin++){
+				lumi += hCounterTCE->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by TCE is: %.2f", lumi);
+		}
+		else Printf("no TCE triggers info");
+		if (hCounterZEM){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterZEM->GetNbinsX();ibin++){
+				lumi += hCounterZEM->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by ZEM is: %.2f", lumi);
+		}
+		else Printf("no ZNC triggers info");
+		if (hCounterZNC){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterZNC->GetNbinsX();ibin++){
+				lumi += hCounterZNC->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by ZNC is: %.2f", lumi);
+		}
+		else Printf("no ZNC triggers info");
+		if (hCounterTVXafterBCcuts){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterTVXafterBCcuts->GetNbinsX();ibin++){
+				lumi += hCounterTVXafterBCcuts->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by TVX afterBCcuts is: %.2f", lumi);
+		}
+		else Printf("no TVX afterBCcuts triggers info");
+		if (hCounterTCEafterBCcuts){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterTCEafterBCcuts->GetNbinsX();ibin++){
+				lumi += hCounterTCEafterBCcuts->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by TCE afterBCcuts is: %.2f", lumi);
+		}
+		else Printf("no TCE afterBCcuts triggers info");
+		if (hCounterZEMafterBCcuts){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterZEMafterBCcuts->GetNbinsX();ibin++){
+				lumi += hCounterZEMafterBCcuts->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by ZEM afterBCcuts is: %.2f", lumi);
+		}
+		else Printf("no ZNC afterBCcuts triggers info");
+		if (hCounterZNCafterBCcuts){
+			Double_t lumi = 0.0;
+			for (int ibin(1);ibin <= hCounterZNCafterBCcuts->GetNbinsX();ibin++){
+				lumi += hCounterZNCafterBCcuts->GetBinContent(ibin);
+			}
+			Printf("Total triggers as seen by ZNC afterBCcuts is: %.2f", lumi);
+		}
+		else Printf("no ZNC afterBCcuts triggers info");
 	}
 	else Printf("no lumi file found");
 }
